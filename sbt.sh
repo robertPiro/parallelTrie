bf8ea75d8c5e91c626a027f3d258d6a96014b145
Robert Piro <r.e.f.piro@gmail.com>
2019-06-17 06:18:07 +0000

    sbt installation script

--- sbt.sh
+++ sbt.sh
@@ -3,3 +3,27 @@ then
     echo sbt is available
     exit 1
 fi
+
+SBTURL="https://sbt-downloads.cdnedge.bluemix.net/releases/v1.2.8/sbt-1.2.8.tgz"
+TARGZ=${SBTUR##*/}
+OLDPWD=$PWD
+
+echo "Installing $TARGZ locally in home directory"
+
+cd $HOME
+if ! [ -d $HOME/bin ];
+then
+    mkdir -p $HOME/bin
+    echo 'PATH=$PATH:~/bin' > $HOME/.bashrc
+fi
+
+if ![  -f $TARGZ ]
+then
+    curl $SBTURL
+fi
+
+tar xvzf $TARGZ
+ls -s $HOME/sbt/bin/sbt $HOME/bin/sbt
+
+cd $OLDPWD
+echo "Restart your terminal!"
